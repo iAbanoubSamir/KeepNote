c3a206a5780d38c0fd8430e9093562751defcbcd
Abanoub Samir <abanoub.sameer.22@gmail.com>
2022-10-10 19:43:20 +0200

    Move to ui/view/main

--- app/src/main/java/com/abanoub/samir/keep/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/abanoub/samir/keep/ui/view/main/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.abanoub.samir.keep.ui.main
+package com.abanoub.samir.keep.ui.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.abanoub.samir.keep.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
