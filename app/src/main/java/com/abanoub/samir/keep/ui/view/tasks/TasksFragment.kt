6cb838e255df0d1359fb6807346da5e509ebd2f7
Abanoub Samir <abanoub.sameer.22@gmail.com>
2022-10-10 23:13:48 +0200

    implemented search functionality

--- app/src/main/java/com/abanoub/samir/keep/ui/view/tasks/TasksFragment.kt
+++ app/src/main/java/com/abanoub/samir/keep/ui/view/tasks/TasksFragment.kt
@@ -1,13 +1,18 @@
 package com.abanoub.samir.keep.ui.view.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.abanoub.samir.keep.R
 import com.abanoub.samir.keep.databinding.FragmentTasksBinding
 import com.abanoub.samir.keep.ui.adapter.TasksAdapter
+import com.abanoub.samir.keep.utils.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,6 +36,41 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
         viewModel.tasks.observe(viewLifecycleOwner) {
             tasksAdapter.submitList(it)
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_search -> {
+                true
+            }
+            R.id.action_sort_by_name -> {
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
