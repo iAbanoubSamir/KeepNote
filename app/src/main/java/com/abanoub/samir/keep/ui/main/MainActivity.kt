395da47329c8cdf2d7c0ca535750dd2f51caeae4
Abanoub Samir <abanoub.sameer.22@gmail.com>
2022-10-10 18:26:15 +0200

    Setup Navigation Component

--- app/src/main/java/com/abanoub/samir/keep/ui/main/MainActivity.kt
+++ app/src/main/java/com/abanoub/samir/keep/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.abanoub.samir.keep.ui
+package com.abanoub.samir.keep.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/abanoub/samir/keep/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.abanoub.samir.keep.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.abanoub.samir.keep.R
+
+class TasksFragment: Fragment(R.layout.fragment_tasks) {
+}
