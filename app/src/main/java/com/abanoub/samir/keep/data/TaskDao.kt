6aa91dd92affa3eef6f3c0d0ba3cb48c27e3678b
Abanoub Samir <abanoub.sameer.22@gmail.com>
2022-10-11 17:55:53 +0200

    implemented sort functionality

--- app/src/main/java/com/abanoub/samir/keep/data/TaskDao.kt
+++ app/src/main/java/com/abanoub/samir/keep/data/TaskDao.kt
@@ -1,6 +1,7 @@
 package com.abanoub.samir.keep.data
 
 import androidx.room.*
+import com.abanoub.samir.keep.ui.view.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -15,7 +16,19 @@ interface TaskDao {
     @Delete()
     suspend fun delete(task: Task)
 
-    @Query("SELECT * FROM tasks_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getTasks(searchQuery: String): Flow<List<Task>>
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when (sortOrder) {
+            SortOrder.BY_DATE -> {
+                getTasksSortedByDateCreated(query, hideCompleted)
+            }
+            SortOrder.BY_NAME -> {
+                getTasksSortedByName(query, hideCompleted)
+            }
+        }
 
+    @Query("SELECT * FROM tasks_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("SELECT * FROM tasks_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, createdAt")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
 }
